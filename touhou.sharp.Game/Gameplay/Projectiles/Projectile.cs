e5fe6cbd7aa6d3cfbd24bd82c68530188907ba7c
Shawdooow <banhammer1915@gmail.com>
2018-06-03 22:05:18 -0400

    add some base properties to `Projectile`

--- touhou.sharp.Game/Gameplay/Projectiles/Projectile.cs
+++ touhou.sharp.Game/Gameplay/Projectiles/Projectile.cs
@@ -1,7 +1,19 @@
-﻿namespace touhou.sharp.Game.Gameplay.Projectiles
+﻿using OpenTK;
+using OpenTK.Graphics;
+using Symcol.Core.GameObjects;
+
+namespace touhou.sharp.Game.Gameplay.Projectiles
 {
     public abstract class Projectile
     {
+        public int Team { get; set; }
 
+        public double Damage { get; set; } = 10;
+
+        public Vector2 Size { get; set; } = new Vector2(10);
+
+        public Shape Shape { get; set; }
+
+        public Color4 Color { get; set; }
     }
 }
