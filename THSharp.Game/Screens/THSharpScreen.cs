9a8c5ed44762e337eb2b61198543eb12b507a000
Shawdooow <banhammer1915@gmail.com>
2018-06-07 00:26:47 -0400

    add overridecursor property to screen

--- THSharp.Game/Screens/THSharpScreen.cs
+++ THSharp.Game/Screens/THSharpScreen.cs
@@ -1,13 +1,19 @@
-﻿using osu.Framework.Screens;
+﻿using osu.Framework.Graphics.Cursor;
+using osu.Framework.Screens;
 
 namespace THSharp.Game.Screens
 {
     public class THSharpScreen : Screen
     {
         /// <summary>
+        /// Whether the toolbar is visible by default in this screen
+        /// </summary>
+        protected virtual bool ShowToolBar => true;
+
+        /// <summary>
         /// Currently this does nothing but in the future this will apply whenever this screen is current
         /// </summary>
-        public virtual bool ShowToolBar => true;
+        protected virtual CursorContainer OverrideCursor => null;
 
         protected override void OnEntering(Screen last)
         {
